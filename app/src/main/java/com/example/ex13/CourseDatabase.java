636a4aea33c3bd1fe9be6746e68b1309f2b3e9af
Le <nevergiveup2025.qn@gmail.com>
2024-06-16 17:32:04 +0700

    slot 10

---
+++ app/src/main/java/com/example/ex13/CourseDatabase.java
@@ -0,0 +1,21 @@
+package com.example.ex13;
+
+import android.content.Context;
+import androidx.room.Room;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Course.class}, version = 1)
+public abstract class CourseDatabase extends RoomDatabase {
+    private static CourseDatabase instance;
+    public abstract CourseDao courseDao();
+    public static synchronized CourseDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), CourseDatabase.class, "course_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+        }
+        return instance;
+    }
+}
+
